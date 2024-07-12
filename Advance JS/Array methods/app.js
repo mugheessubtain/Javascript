5a62b2f12f1d4b34a69947183356085bd2f2b156
mugheessubtain <mugheessubtain@gmail.com>
2024-07-12 22:01:03 +0500

    Practice of Javascript

--- Advance JS/Array methods/app.js
+++ Advance JS/Array methods/app.js
@@ -55,3 +55,105 @@ let str2 = "Saylani Mass Information Technology";
 let abbr2=str1.split(" ")[0]
 console.log(abbr2[0]);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// lab class nine
+
+
+let ipEl = document.querySelectorAll(".count-input");
+let h1El = document.querySelectorAll(".msg");
+let errorEl = document.querySelectorAll(".error-msg");
+
+
+function showMsg() {
+    let formVal = ipEl[0].value
+    if (isNaN(formVal)) {
+        // console.error("You must enter a number");
+        errorEl[0].innerHTML = "You must enter a number"
+        errorEl[0].style.borderWidth = "1px";
+        // errorEl[0].style.backgroundColor="black"
+    }
+    else {
+        // console.log(formVal);
+        h1El[0].innerHTML = Number(h1El[0].innerHTML) + Number(formVal);
+        errorEl[0].innerHTML = "";
+        errorEl[0].style.borderWidth = "0px";
+
+
+
+    }
+}
+function abc() {
+    let formVal = ipEl[0].value
+    if (isNaN(formVal)) {
+        // console.error("You must enter a number");
+        errorEl[0].innerHTML = "You must enter a number"
+        errorEl[0].style.borderWidth = "1px";
+    }
+    else {
+        // console.log(formVal);
+        h1El[0].innerHTML = Number(h1El[0].innerHTML) - Number(formVal);
+        errorEl[0].innerHTML = "";
+        errorEl[0].style.borderWidth = "0px";
+    }
+}
+function clr() {
+    ipEl[0].value="1";
+    errorEl[0].innerHTML = "";
+    h1El[0].innerHTML="";
+    errorEl[0].style.borderWidth = "0px";
+
+    
+}
+
+
+
+
+// index.html
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <link rel="stylesheet" href="style.css">
+// </head>
+// <body>
+//     <h1>Count Game</h1>
+//     <div class="count-box">
+
+//         <input type="text" name="count-input" value="1" class="count-input">
+//     </div>
+//     <div class="count-box">
+//         <button onclick="showMsg()" class="plus">+</button>
+//         <button onclick="abc()" class="minus">-</button>
+//         <button onclick="clr()" class="clear">C</button>
+
+
+//     </div>
+//         <h1 class="msg"></h1>
+//         <h3 class="error-msg"></h3>
+//         <script src="app.js"></script>
+// </body>
+// </html>
